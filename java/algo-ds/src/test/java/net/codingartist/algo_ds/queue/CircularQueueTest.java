a19aeb77e751777d9378bd3f2909922ef7e740bf
shin <shin@ubuntu>
2020-11-05 00:34:48 -0800

    clean up

--- java/algo-ds/src/test/java/net/codingartist/algo_ds/queue/CircularQueueTest.java
+++ java/algo-ds/src/test/java/net/codingartist/algo_ds/queue/CircularQueueTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import net.codingartist.algo_ds.unionfind.UnionFind;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
