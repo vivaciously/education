da9ac2f7d4c09aa0c6a3d09a8dc73d73f31211b7
shin <shin@ubuntu>
2020-11-15 01:30:59 -0800

    clean up

--- java/algo-ds/src/test/java/net/codingartist/algo_ds/utils/BitTestUtilsTest.java
+++ java/algo-ds/src/test/java/net/codingartist/algo_ds/utils/BitTestUtilsTest.java
@@ -2,7 +2,6 @@ package net.codingartist.algo_ds.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class BitTestUtilsTest {
