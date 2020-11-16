ddebcfa9588f3acf661a64c778c516c571ab4e37
shin <shin@ubuntu>
2020-11-16 00:44:21 -0800

    Added more methods

--- java/algo-ds/src/main/java/net/codingartist/algo_ds/utils/ArrayTestUtils.java
+++ java/algo-ds/src/main/java/net/codingartist/algo_ds/utils/ArrayTestUtils.java
@@ -1,5 +1,7 @@
 package net.codingartist.algo_ds.utils;
 
+import java.util.Arrays;
+
 public class ArrayTestUtils {
 
 	private ArrayTestUtils() {
@@ -37,5 +39,103 @@ public class ArrayTestUtils {
 		}
 		return results;
 	}
+	
+	public static Integer[] strToIntegerArray(String str) {
+		return Arrays.stream(strToIntArray(str)).boxed().toArray(Integer[]::new);
+	}
+	
+	public static Integer[] intToIntegerArray(int[] array) {
+		return Arrays.stream(array).boxed().toArray(Integer[]::new);
+	}
+	
+	public static int[] integerToIntArray(Integer[] array) {
+		return Arrays.stream(array).mapToInt(Integer::valueOf).toArray();
+	}
+	
+	
+	public static char[] strToCharArray(String str) {
+		if(str == null) {
+			throw new NullPointerException("Given str is null.");
+		}
+		str = str.replaceAll("\s+", "");
+		if(str.length() < 2) {
+			throw new IllegalArgumentException("The format of str is '[a,b,c]' where it starts with '[' and ends with ']'"
+					+ "and 'a' 'b' 'c' are char values and separated by a comma.");
+		}
+		if(!(str.startsWith("[") && str.endsWith("]"))) {
+			throw new IllegalArgumentException("The format of str is '[a,b,c]' where it starts with '[' and ends with ']'"
+					+ "and 'a' 'b' 'c' are char values and separated by a comma.");
+		}
+		str = str.substring(1, str.length()-1);
+		if(str.length() == 0) {
+			return new char[] {};
+		}
+		for(char c : str.toCharArray()) {
+			if(!(Character.isLetter(c) ||  c == ',' || c == '\'')) {
+				throw new IllegalArgumentException("The format of str is '[a,b,c]' where it starts with '[' and ends with ']'"
+						+ "and 'a' 'b' 'c' are char values and separated by a comma.");
+			}
+		}
+		String[] data = str.split(",");
+		char[] results = new char[data.length];
+		int index = 0;
+		for(String s : data) {
+			char c = s.charAt(0);
+			if(c =='\'') {
+				results[index++] = ' ';
+			} else {
+				results[index++] = c;
+			}
+			
+		}
+		return results;
+	}
+	
+	public static Character[] strToCharacterArray(String str) {
+		char[] chars = strToCharArray(str);
+		Character[] results = new Character[chars.length];
+		int index = 0;
+		for(char c : chars) {
+			results[index++] = Character.valueOf(c);
+		}
+		chars = null;
+		return results;
+	}
+	
+	public static String[] strToStringArray(String str) {
+		if(str == null) {
+			throw new NullPointerException("Given str is null.");
+		}
+		str = str.replaceAll("\s+", "");
+		if(str.length() < 2) {
+			throw new IllegalArgumentException("The format of str is '[a,b,c]' where it starts with '[' and ends with ']'"
+					+ "and 'a' 'b' 'c' are char values and separated by a comma.");
+		}
+		if(!(str.startsWith("[") && str.endsWith("]"))) {
+			throw new IllegalArgumentException("The format of str is '[a,b,c]' where it starts with '[' and ends with ']'"
+					+ "and 'a' 'b' 'c' are char values and separated by a comma.");
+		}
+		str = str.substring(1, str.length()-1);
+		if(str.length() == 0) {
+			return new String[] {};
+		}
+		str = str.replace("''", "'");
+		String[] data = str.split(",");
+		String[] results = new String [data.length];
+		int index = 0;
+		for(String s : data) {
+			char c = s.charAt(0);
+			if(s.length() == 1 && c =='\'') {
+				results[index++] = "";
+			} else if(s.contains("'")){
+				results[index++] = s.replace('\'', ' ');
+			} else {
+				results[index++] = s;
+			}
+			
+		}
+		return results;
+	}
+	
 	
 }
--- java/algo-ds/src/test/java/net/codingartist/algo_ds/utils/ArrayTestUtilsTest.java
+++ java/algo-ds/src/test/java/net/codingartist/algo_ds/utils/ArrayTestUtilsTest.java
@@ -51,4 +51,207 @@ public class ArrayTestUtilsTest {
 			ArrayTestUtils.strToIntArray(null);
 		});
 	}
+	
+	@Test
+	public void testStrToIntegerArray() {
+		String input ="[0, 1, 2]";
+		Integer[] result = ArrayTestUtils.strToIntegerArray(input);
+		for(int i=0; i<result.length; i++) {
+			assertEquals(result[i], i);
+		}
+		input = " []";
+		result = ArrayTestUtils.strToIntegerArray(input);
+		assertTrue(result.length == 0);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("(1,2,3)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("[1,2,3)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("(1,2,3]");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("abc");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("ab");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("[a,b,c]");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToIntegerArray("['']");
+		});
+		
+		assertThrows(NullPointerException.class, () -> {
+			ArrayTestUtils.strToIntegerArray(null);
+		});
+	}
+	
+	@Test
+	public void testIntToIntegerArray() {
+		int[] original = new int[]{1,2,3,4,5};
+		Integer[] results = ArrayTestUtils.intToIntegerArray(original);
+		for(int i=0; i<original.length; i++) {
+			assertEquals(original[i], results[i]);
+		}
+	}
+	
+	@Test
+	public void testIntegerToIntArray() {
+		Integer[] original = new Integer[] {1,2,3};
+		int[] results = ArrayTestUtils.integerToIntArray(original);
+		for(int i=0; i<original.length; i++) {
+			assertEquals(original[i], results[i]);
+		}
+	}
+	
+	@Test
+	public void testStrToCharArray() {
+		String input ="[A, B, C, ' ', D]";
+		char[] result = ArrayTestUtils.strToCharArray(input);
+		char[] test = {'A','B','C', ' ', 'D'};//' ' is a space
+		for(int i=0; i<result.length; i++) {
+			assertEquals(result[i], test[i]);
+		}
+		input = " []";
+		result = ArrayTestUtils.strToCharArray(input);
+		assertTrue(result.length == 0);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("(a,b,c)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("[a,b,c)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("(a,b,c]");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("abc");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("ab");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharArray("[1,2,4]");
+		});
+		
+		assertThrows(NullPointerException.class, () -> {
+			ArrayTestUtils.strToCharArray(null);
+		});
+	}
+	
+	@Test
+	public void testStrToCharacterArray() {
+		String input ="[A, B, C, ' ', D]";
+		Character[] result = ArrayTestUtils.strToCharacterArray(input);
+		char[] test = {'A','B','C', ' ', 'D'};//' ' is a space
+		for(int i=0; i<result.length; i++) {
+			assertEquals(result[i], test[i]);
+		}
+		input = " []";
+		result = ArrayTestUtils.strToCharacterArray(input);
+		assertTrue(result.length == 0);
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("(a,b,c)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("[a,b,c)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("(a,b,c]");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("abc");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("ab");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("[1,2,4]");
+		});
+		
+		assertThrows(NullPointerException.class, () -> {
+			ArrayTestUtils.strToCharacterArray(null);
+		});
+	}
+	
+	@Test
+	public void testStrToStringArray() {
+		String input = "[AB, a, b, c, A, ' ', D]";
+		String[] results = ArrayTestUtils.strToStringArray(input);
+		String[] expected = new String[] {"AB", "a", "b", "c", "A", "", "D"};
+		for(int i=0; i<results.length; i++) {
+			assertEquals(results[i], expected[i]);
+		}
+		results = ArrayTestUtils.strToStringArray("[] ");
+		assertTrue(results.length == 0);
+		expected = new String[] {"1", "2", "@", "+", "ABCDEFG", "CD 67" , " Abc f"};
+		results = ArrayTestUtils.strToStringArray("[1,2,@,+, ABCDEFG, CD' '67, ' 'Abc' 'f]");
+		for(int i=0; i<results.length; i++) {
+			assertEquals(results[i], expected[i]);
+		}
+		
+		assertThrows(NullPointerException.class, () -> {
+			ArrayTestUtils.strToStringArray(null);
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToCharacterArray("(a,b,c)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToStringArray("[a,b,c)");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToStringArray("(a,b,c]");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToStringArray("abc");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToStringArray("ab");
+		});
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			ArrayTestUtils.strToStringArray("");
+		});
+		
+	}
+	
 }
